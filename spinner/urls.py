9c749874954fb5570a5084456aba6048949decd5
Vaibhavdesh20 <111379850+Vaibhavdesh20@users.noreply.github.com>
2023-12-12 12:27:10 +0000

    change

--- spinner/urls.py
+++ spinner/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('index',views.index,name='index'),
     path('contact',views.contact,name='contact'),
     path('about',views.about,name='about'),
+    path('member',views.member,name='member'),
 ]
