aa3334eff7246cf1bd7f569d45a721f56381132d
Vaibhavdesh20 <111379850+Vaibhavdesh20@users.noreply.github.com>
2024-01-04 19:30:56 +0000

    ch

--- spin/settings.py
+++ spin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%a(g$#9o)zt5^i^@xmhuw$3mk%+wxf$mxgp*e0-l!spo2qfou3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'spin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
