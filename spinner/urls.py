517c87589b66b8370b8c5aa2e0ea0b658984a3bb
Vaibhavdesh20 <111379850+Vaibhavdesh20@users.noreply.github.com>
2024-01-04 18:06:16 +0000

    change

--- spinner/urls.py
+++ spinner/urls.py
@@ -5,7 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.index,name='index'),
     path('index',views.index,name='index'),
-    path('contact',views.contact,name='contact'),
-    path('about',views.about,name='about'),
     path('member',views.member,name='member'),
+    path('prashant',views.prashant,name='prashant'),
+    path('member2',views.member2,name='member2'),
+    path('member3',views.member3,name='member3'),
+    path('history',views.history,name='history'),
+    path('services',views.services,name='services'),
 ]
--- spinner/views.py
+++ spinner/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
-def contact(request):
-     return render(request,'contact.html')
-
-def about(request):
-     return render(request,'about.html')
-
 def member(request):
      return render(request,'member.html')
+
+def prashant(request):
+     return render(request,'prashant.html')
+
+def member2(request):
+     return render(request,'member2.html')
+
+def history(request):
+     return render(request,'history.html')
+
+def member3(request):
+     return render(request,'member3.html')
+
+def services(request):
+     return render(request,'services.html')
